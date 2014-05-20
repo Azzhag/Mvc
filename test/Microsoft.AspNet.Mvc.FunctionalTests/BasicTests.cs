c84ed37175350877defd3cc6b9a3b3d4e9e32cce
Javier Calvarro Nelson <jacalvar@microsoft.com>
2014-05-19 22:16:30 -0700

    Added open source heading to the files. Cleaned up files.

--- test/Microsoft.AspNet.Mvc.FunctionalTests/BasicTests.cs
+++ test/Microsoft.AspNet.Mvc.FunctionalTests/BasicTests.cs
@@ -1,23 +1,22 @@
 ﻿// Copyright (c) Microsoft Open Technologies, Inc. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
+using System;
+using System.IO;
+using System.Reflection;
+using System.Threading.Tasks;
+using BasicWebSite;
 using Microsoft.AspNet.Builder;
 using Microsoft.AspNet.TestHost;
 using Microsoft.Framework.DependencyInjection;
 using Microsoft.Framework.DependencyInjection.Fallback;
 using Microsoft.Framework.Runtime;
 using Microsoft.Framework.Runtime.Infrastructure;
-using System;
-using System.IO;
-using System.Reflection;
-using System.Runtime.Versioning;
-using System.Threading.Tasks;
-using BasicWebSite;
 using Xunit;
 
 namespace Microsoft.AspNet.Mvc.FunctionalTests
 {
-    public class HomeScenarioTests
+    public class BasicTests
     {
         private readonly IServiceProvider _provider;
         private readonly Action<IBuilder> _app = new Startup().Configure;
@@ -25,10 +24,10 @@ namespace Microsoft.AspNet.Mvc.FunctionalTests
         // Some tests require comparing the actual response body against an expected response baseline
         // so they require a reference to the assembly on which the resources are located, in order to
         // make the tests less verbose, we get a reference to the assembly with the resources and we
-        // use it on all the resto f the tests.
-        private readonly Assembly _resourcesAssembly = typeof(HomeScenarioTests).GetTypeInfo().Assembly;
+        // use it on all the rest of the tests.
+        private readonly Assembly _resourcesAssembly = typeof(BasicTests).GetTypeInfo().Assembly;
 
-        public HomeScenarioTests()
+        public BasicTests()
         {
             var originalProvider = CallContextServiceLocator.Locator.ServiceProvider;
             IApplicationEnvironment appEnvironment = originalProvider.GetService<IApplicationEnvironment>();
@@ -41,12 +40,12 @@ namespace Microsoft.AspNet.Mvc.FunctionalTests
             // test.
             string appBasePath = CalculateApplicationBasePath(appEnvironment);
             _provider = new ServiceCollection()
-                .AddInstance(typeof(IApplicationEnvironment), new MockApplicationEnvironment(appEnvironment, appBasePath))
+                .AddInstance(typeof(IApplicationEnvironment), new TestApplicationEnvironment(appEnvironment, appBasePath))
                 .BuildServiceProvider(originalProvider);
         }
 
         [Fact]
-        public async Task CanRenderMainView()
+        public async Task CanRender_Views()
         {
             // Arrange
             var server = TestServer.Create(_provider, _app);
@@ -63,53 +62,17 @@ namespace Microsoft.AspNet.Mvc.FunctionalTests
             Assert.Equal(expectedContent, responseContent);
         }
 
-        // Represents an application environment that overrides the base path of the original
-        // application environment in order to make it point to the folder of the original web
-        // aplication so that components like ViewEngines can find views as if they were executing
-        // in a regular context.
-        private class MockApplicationEnvironment : IApplicationEnvironment
-        {
-            private readonly IApplicationEnvironment _originalAppEnvironment;
-            private readonly string _applicationBasePath;
-
-            public MockApplicationEnvironment(IApplicationEnvironment originalAppEnvironment, string appBasePath)
-            {
-                _originalAppEnvironment = originalAppEnvironment;
-                _applicationBasePath = appBasePath;
-            }
-
-            public string ApplicationName
-            {
-                get { return _originalAppEnvironment.ApplicationName; }
-            }
-
-            public string Version
-            {
-                get { return _originalAppEnvironment.Version; }
-            }
-
-            public string ApplicationBasePath
-            {
-                get { return _applicationBasePath; }
-            }
-
-            public FrameworkName TargetFramework
-            {
-                get { return _originalAppEnvironment.TargetFramework; }
-            }
-        }
-
         // Calculate the path relative to the current application base path. 
         private static string CalculateApplicationBasePath(IApplicationEnvironment appEnvironment)
         {
-            // Mvc/test/MvcSample.Test
+            // Mvc/test/Microsoft.AspNet.Mvc.FunctionalTests
             var appBase = appEnvironment.ApplicationBasePath;
 
             // Mvc/test
             var test = Path.GetDirectoryName(appBase);
 
-            // Mvc/Samples/MvcSample.Web
-            return Path.GetFullPath(Path.Combine(appBase, "..", "Websites", "BasicWebSite"));
+            // Mvc/test/WebSites/BasicWebSite
+            return Path.GetFullPath(Path.Combine(appBase, "..", "WebSites", "BasicWebSite"));
         }
     }
 }
